ac77a0d86553d72f29637c6a422685b1a89db314
mor8cho <106899110+mor8cho@users.noreply.github.com>
2022-06-06 23:10:10 -0600

    some changes dont ask

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,6 @@
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
 */
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
